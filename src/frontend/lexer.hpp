457c6921d782f83b11e71d39e0ef6528868adb1d
Yash-2907 <guptayp_1@rknec.edu>
2025-06-10 03:16:12 +0530

    lexer understands token type

--- src/frontend/lexer.hpp
+++ src/frontend/lexer.hpp
@@ -8,11 +8,12 @@ typedef enum
 {
     token_insert,
     token_into,
-    token_string,
     token_value,
+    token_string,
+    token_integer,
     token_left_paren,
     token_right_paren,
-    token_int
+    token_comma
 } token_set;
 
 struct token
@@ -30,6 +31,31 @@ private:
     char current;
     std::vector<token> token_list;
 
+    std::string token_type_to_string(token_set local_token_set)
+    {
+        switch (local_token_set)
+        {
+        case 0:
+            return "token_insert";
+        case 1:
+            return "token_into";
+        case 2:
+            return "token_value";
+        case 3:
+            return "token_string";
+        case 4:
+            return "token_integer";
+        case 5:
+            return "token_left_parenthesis";
+        case 6:
+            return "token_right_paranthesis";
+        case 7:
+            return "token_comma";
+        default:
+            return "[!] ERROR IDENTIFYING TOKEN -> " + (local_token_set);
+        }
+    }
+
     void advance()
     {
         if (cursor == length - 1)
@@ -40,7 +66,7 @@ private:
         else
         {
             cursor++;
-            current=input_buffer[cursor];
+            current = input_buffer[cursor];
         }
     }
 
@@ -54,13 +80,36 @@ private:
     {
         std::string local_buffer;
         token local_token;
-        while(isalpha(current))
+        while (isalpha(current))
         {
             local_buffer.push_back(current);
             advance();
         }
-        local_token.token_type=token_string;
-        local_token.value=local_buffer;
+        local_token.token_type = token_string;
+        local_token.value = local_buffer;
+        token_list.push_back(local_token);
+    }
+
+    void tokenize_integer()
+    {
+        std::string local_buffer;
+        token local_token;
+        while (isdigit(current))
+        {
+            local_buffer.push_back(current);
+            advance();
+        }
+        local_token.token_type = token_integer;
+        local_token.value = local_buffer;
+        token_list.push_back(local_token);
+    }
+
+    void tokenize_special_char(token_set type)
+    {
+        token local_token;
+        local_token.token_type = type;
+        local_token.value = current;
+        advance();
         token_list.push_back(local_token);
     }
 
@@ -68,9 +117,9 @@ private:
     {
         input_buffer.clear();
         token_list.clear();
-        cursor=0;
-        length=0;
-        input_buffer='\0';
+        cursor = 0;
+        length = 0;
+        input_buffer = '\0';
     }
 
 public:
@@ -81,7 +130,7 @@ public:
     void displayToken()
     {
         for (auto &it : token_list)
-            std::cout << it.token_type << ") " << it.value << std::endl;
+            std::cout << it.value << " [" << token_type_to_string(it.token_type) << "]" << std::endl;
     }
 
     void initialize(std::string input_buffer)
@@ -97,9 +146,39 @@ public:
         while (current)
         {
             skip_whitespace();
-            if(isalpha(current))
+            if (isalpha(current))
             {
                 tokenize_alpha();
+            }
+            else if (isdigit(current))
+            {
+                tokenize_integer();
+            }
+            else
+            {
+                switch (current)
+                {
+                case '(':
+                {
+                    tokenize_special_char(token_left_paren);
+                    break;
+                }
+                case ')':
+                {
+                    tokenize_special_char(token_right_paren);
+                    break;
+                }
+                case ',':
+                {
+                    tokenize_special_char(token_comma);
+                    break;
+                }
+                default:
+                {
+                    std::cout << "[!] ERROR : UNINDENTIFIED CHARACTER -> " << current<<std::endl;
+                    advance();
+                }
+                }
             }
         }
         displayToken();
